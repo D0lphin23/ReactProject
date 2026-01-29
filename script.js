6f0d3b81ab435456d434df0d1233553345e51fbf
gorelik.e <gorelikev23@gmail.com>
2026-01-29 22:39:14 +0500

    - Вывод в консоль типов переменных - Вывод в консоль длинны строки - Вывод в консоль конкатенацию строк - Вывод в консоль переменных с использованием методов строк

---
+++ script.js
@@ -0,0 +1,33 @@
+const title = 'ReactProject';
+const screens = 'Простые, Сложные, Интерактивные, Очень сложные, Гиперсложные';
+const screenPrice = 50;
+const rollback = 100;
+const fullPrice = 350;
+const adaptive = true;
+
+// Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+// Вывести в консоль длину строки из переменной screens;
+console.log(screens.length);
+
+// Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани”
+//  и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
+const layoutScreenPrice = `Стоимость верстки экранов ${screenPrice} песо`;
+const developmentFullPrice = "Стоимость разработки сайта" + " " + fullPrice + " " + "песо";
+
+console.log(layoutScreenPrice);
+console.log(developmentFullPrice);
+
+// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+const toLowerScreens = screens.toLowerCase().split(', ');
+
+console.log(toLowerScreens);
+
+// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
+// Кажеться что формула в задании не верная, для вычесления процента. По этому я использовал другую формулу.
+const percentRollback = (rollback * 100) / fullPrice;
+
+console.log(percentRollback);
