f603102d7e2839ce3a047692d2550804b5e6937a
gorelik.e <gorelikev23@gmail.com>
2026-02-03 23:43:59 +0500

    - Промежуточный вариант выполнения ДЗ 4

--- script.js
+++ script.js
@@ -3,17 +3,53 @@
 const title = prompt('Как называется ваш проект?');
 const screens = prompt('Какие типы экранов нужно разработать? (Перечислите через запятую)', 'Простые, Сложные, Интерактивные');
 const screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?'));
-const adaptive = prompt('Нужен ли адаптив на сайте?', 'Да/Нет').toLowerCase() === 'да';
+const adaptive = confirm('Нужен ли адаптив на сайте?');
 const service1 = prompt('Какой дополнительный тип услуги нужен?');
 const servicePrice1 = parseFloat(prompt('Сколько это будет стоить?'));
 const service2 = prompt('Какой дополнительный тип услуги нужен?');
 const servicePrice2 = parseFloat(prompt('Сколько это будет стоить?'));
 const rollback = 15;
 
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+let layoutScreenPrice = '';
+let developmentFullPrice = '';
+let toLowerScreens = [];
+let percentRollback = 0;
+let fullPrice = 0;
+let servicePercentPrice = 0;
+let allServicePrices = 0;
+let formatedTitle = '';
 
-console.log(servicePercentPrice);
+
+const getAllServicePrices = function(addService1, addService2) {
+    return addService1 + addService2;
+}
+
+const getTitle = function(nameProject) {
+    let deleteSpaces = '';
+
+    deleteSpaces = nameProject.trim();
+
+    return deleteSpaces[0].toUpperCase() + deleteSpaces.slice(1).toLowerCase();
+
+}
+
+function getFullPrice(layoutСost, allServicePrices) {
+    return layoutСost + allServicePrices;
+}
+
+allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+
+fullPrice = getFullPrice(screenPrice, allServicePrices);
+
+formatedTitle = getTitle(title);
+
+// Привести строку screens к нижнему регистру и разбить строку на массив
+toLowerScreens = screens.toLowerCase().split(', ');
+
+// Процент отката посреднику за работу (fullPrice * (rollback/100))
+percentRollback = (fullPrice * (rollback / 100));
+
+servicePercentPrice = Math.ceil(fullPrice - percentRollback);
 
 if (fullPrice >= 30000) {
     console.log('Даем скидку в 10%');
@@ -25,30 +61,18 @@ if (fullPrice >= 30000) {
     console.log('Что-то пошло не так');
 }
 
+// “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани”
+//  и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
+layoutScreenPrice = `Стоимость верстки экранов ${screenPrice} песо`;
+developmentFullPrice = "Стоимость разработки сайта" + " " + fullPrice + " " + "песо";
 
-
-// Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
-
-// Вывести в консоль длину строки из переменной screens;
 console.log(screens.length);
-
-// Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани”
-//  и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
-const layoutScreenPrice = `Стоимость верстки экранов ${screenPrice} песо`;
-const developmentFullPrice = "Стоимость разработки сайта" + " " + fullPrice + " " + "песо";
-
 console.log(layoutScreenPrice);
 console.log(developmentFullPrice);
-
-// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
-const toLowerScreens = screens.toLowerCase().split(', ');
-
 console.log(toLowerScreens);
-
-// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
-const percentRollback = (fullPrice * (rollback / 100));
-
 console.log(percentRollback);
+console.log(servicePercentPrice);
+
